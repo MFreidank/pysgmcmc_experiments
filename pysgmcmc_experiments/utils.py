d75a5f51431089509fdbb6ce1ec8ee4fed722816
MFreidank <freidankm@yahoo.de>
2018-04-30 18:33:37 +0200

    refactoring: moved init_random_uniform to utils

--- pysgmcmc_experiments/utils.py
+++ pysgmcmc_experiments/utils.py
@@ -1,6 +1,8 @@
 from subprocess import check_output
 import re
 
+import numpy as np
+
 
 def package_versions():
     package_list = check_output(("pip", "list")).decode().split("\n")[2:]
@@ -17,3 +19,14 @@ def package_versions():
             package, version = match.group(1), match.group(2)
             packages.append((package.strip(), version.strip()))
     return packages
+
+
+def init_random_uniform(lower, upper, num_points, rng=None):
+    if rng is None:
+        rng = np.random.RandomState(np.random.randint(0, 10000))
+
+    n_dims = lower.shape[0]
+
+    return np.array(
+        [rng.uniform(lower, upper, n_dims) for _ in range(num_points)]
+    )
