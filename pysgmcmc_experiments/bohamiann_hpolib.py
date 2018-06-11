4aacda6f2a5af9a32bd6e8e8315568c20032ee34
MFreidank <freidankm@yahoo.de>
2018-06-11 15:52:44 +0200

    roadmap to have hpolib benchmarks here

---
+++ pysgmcmc_experiments/bohamiann_hpolib.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding: iso-8859-15 -*-
+""" Implement a sacred experiment to optimize hpolib functions with SGHMCHD - Bohamiann """
+# XXX: Fork robo and add keras bnn as robo model
+# XXX: Make hpolib a dependency and make sure it is installed *after* pysgmcmc
+# so that the matching theano versions for hpolib remain after pip install
+# XXX: Set up experiment to run all hpolib benchmarks and deploy it on the cluster.
