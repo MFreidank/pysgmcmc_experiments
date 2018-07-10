f3827394857e845c7ef249df1f08027c69b8a266
MFreidank <freidankm@yahoo.de>
2018-07-10 11:43:55 +0200

    changes to energy functions experiment
    New (3.4.1) pymc3 requires explicit
    init for NUTS and HMC to work without throwing
    errors. We rely now on default initialization using
    "jitter + adapt_diag" as used by `pymc3.sample`
    when auto-assigning. This also makes HMC significantly
    more robust in our experiments (but also means we do not
    exactly compare to `vanilla HMC` anymore..).

--- pysgmcmc_experiments/energy_functions.py
+++ pysgmcmc_experiments/energy_functions.py
@@ -2,17 +2,28 @@
 # stepsizes on some standard functions (no noise?). See also MCMCVisualizer
 # plots and pymc3 plots.
 
+
+# Plot the following:
+# vs NUTS with different initial stepsizes
+# vs HMC with different initial stepsizes
+# vs SGHMC with different initial stepsizes
+# vs SGLD with different initial stepsizes
+
+# other plots definitely should go in appendix unless they have some super-unexpected results
+# => show table with all results (what is a reasonable table format?)
+
 # List of samplers:
 # - SGHMC (sgmcmc, theano) [x]
 # - SGHMCHD (pysgmcmc, keras) [x]
 # - SGLD (pysgmcmc, tensorflow) [x] <- from tensorflow_probability
-# - HMC(pymc3)
-# - NUTS (pymc3, pymc3) <- compare to NUTS here as well!
-# - slice/metropolis (pymc3)
+# - HMC(pymc3) [x]
+# - NUTS (pymc3, pymc3) <- compare to NUTS here as well! [x]
+# - slice/metropolis (pymc3) [x]
 
 # Left to add:
 # - SVGD (?)
-# - ?
+# - RelSGHMC (re-implement as optimizer in pysgmcmc)
+# -
 
 import sys
 from os.path import dirname, join as path_join
@@ -35,6 +46,29 @@ from pysgmcmc.samplers.sgld import SGLDSampler
 
 from pysgmcmc_experiments.experiment_wrapper import to_experiment
 import pymc3 as pm
+
+
+def init_hmc(model, stepsize, init="jitter+adapt_diag", chains=1):
+    from pymc3.step_methods.hmc import quadpotential
+    if init == 'jitter+adapt_diag':
+        start = []
+        for _ in range(chains):
+            mean = {var: val.copy() for var, val in model.test_point.items()}
+            for val in mean.values():
+                val[...] += 2 * np.random.rand(*val.shape) - 1
+            start.append(mean)
+        mean = np.mean([model.dict_to_array(vals) for vals in start], axis=0)
+        var = np.ones_like(mean)
+        potential = quadpotential.QuadPotentialDiagAdapt(
+            model.ndim, mean, var, 10)
+
+        return pm.step_methods.HamiltonianMC(
+            step_scale=stepsize,
+            potential=potential,
+            path_length=1
+        )
+    else:
+        raise NotImplementedError()
 
 
 ENERGY_FUNCTIONS = OrderedDict((
@@ -93,13 +127,31 @@ def get_trace(sampler, stepsize, energy_function, burn_in_steps=3000, sampling_s
     sampler_cls = SAMPLERS[sampler]
 
     if sampler in PYMC3_SAMPLERS:
-        with pm.Model():
+        with pm.Model() as model:
             energy_function_.to_pymc3()
-            if sampler in ("NUTS", "HMC"):
-                step = SAMPLERS[sampler](step_scale=stepsize)
+            if sampler == "NUTS":
+                from pymc3.sampling import init_nuts
+                start, step = init_nuts(
+                    init="auto",
+                    n_init=200000,
+                    model=model,
+                    progressbar=True
+                )
+
+                trace = pm.sample(
+                    sampling_steps + burn_in_steps,
+                    tune=burn_in_steps,
+                    step=step,
+                    chains=1,
+                    start=start
+                )
+            elif sampler == "HMC":
+                step = init_hmc(stepsize=stepsize, model=model)
+                trace = pm.sample(sampling_steps + burn_in_steps, tune=burn_in_steps, step=step, chains=1, init="apapt_diag")
             else:
                 step = SAMPLERS[sampler]()
-            trace = pm.sample(sampling_steps + burn_in_steps, tune=burn_in_steps, step=step)
+                trace = pm.sample(sampling_steps + burn_in_steps, tune=burn_in_steps, step=step, chains=1)
+            # trace = pm.sample(sampling_steps + burn_in_steps, tune=burn_in_steps, step=step, chains=1)
             samples = np.asarray([
                 tuple(step.values())[0]
                 for step in trace
@@ -107,7 +159,10 @@ def get_trace(sampler, stepsize, energy_function, burn_in_steps=3000, sampling_s
             try:
                 num_steps, num_parameters, num_chains = samples.shape
             except ValueError:
-                num_steps, num_parameters, num_chains = (*samples.shape, 1)
+                try:
+                    num_steps, num_parameters, num_chains = (*samples.shape, 1)
+                except ValueError:
+                    num_steps, num_parameters, num_chains = (*samples.shape, 1, 1)
             samples = np.reshape(samples, (num_chains, num_steps, num_parameters))
             return {"samples": samples}
 
